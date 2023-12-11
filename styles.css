2798c59123de097b36d1aa41d7b033c19a6d7d07
Kaushik Raj <78633402+Kaushik3788@users.noreply.github.com>
2023-12-11 09:39:10 +0530

    styles.css

---
+++ styles.css
@@ -0,0 +1,23 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+#app {
+  text-align: center;
+}
+
+#videoIdInput {
+  margin-bottom: 10px;
+}
+
+#player {
+  width: 640px;
+  height: 360px;
+}
